76befde2c70246e4b5cc25bbe6726a953d10a183
Loureiro <mindera@C02Z689TLVCF-ME00547.local>
2024-12-11 10:30:50 +0000

    Initial commit

---
+++ src/academy/mindswap/field/Position.java
@@ -0,0 +1,21 @@
+package academy.mindswap.field;
+
+public class Position {
+
+    private final int x;
+    private final int y;
+
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getCol() {
+        return 0;
+    }
+
+    public int getRow() {
+        return 0;
+    }
+}
